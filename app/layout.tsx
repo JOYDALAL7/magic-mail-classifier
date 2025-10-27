9a43865a582ba92eba76996a3c65eb3b1b97bc70
JOYDALAL7 <djjoy2002@gmail.com>
2025-10-28 01:44:53 +0530

    fix: type annotation for children in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import ClientProvider from "./ClientProvider";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
